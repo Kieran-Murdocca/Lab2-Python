85eb7a1847b0a7f61f273f33a53f3f7d046dbffe
KieranMurdocca <kkm5754@psu.edu>
2020-09-11 02:53:14 +0000

    2 def

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 # Author: Kieran Murdocca kkm5754@psu.edu
 # Collaborator: Reuben Lee wzl128@psu.edu
-# Collaborator:Augustus Perseghin agp5191
+# Collaborator: Augustus Perseghin agp5191
 # Collaborator: Krish Choudhary ksc5496@psu.edu
 # Section: 004
 # Breakout: 18
 # Part 2: lab2-python
-ng=float(input("Enter your CMPSC 131 grade: "))
 def getLetterGrade(ng): 
-  if ng >= float(93): print("Your letter grade for CMPSC 131 is A.")
-  elif ng >= float(90): print("Your letter grade for CMPSC 131 is A-.")
-  elif ng >= float(87): print("Your letter grade for CMPSC 131 is B+.")
-  elif ng >= float(83): print("Your letter grade for CMPSC 131 is B.")
-  elif ng >= float(80): print("Your letter grade for CMPSC 131 is B-.")
-  elif ng >= float(77): print("Your letter grade for CMPSC 131 is C+.")
-  elif ng >= float(70): print("Your letter grade for CMPSC 131 is C.")
-  elif ng >= float(60): print("Your letter grade for CMPSC 131 is D.")
-  else: print("Your letter grade for CMPSC 131 is F.")
-getLetterGrade(ng)
+  if ng >= 93: 
+    return "A"
+  elif ng >= 90: 
+    return "A-"
+  elif ng >= 87: 
+    return "B+"
+  elif ng >= 83: 
+    return "B"
+  elif ng >= 80: 
+    return "B-"
+  elif ng >= 77: 
+    return "C+"
+  elif ng >= 70: 
+    return "C"
+  elif ng >= 60: 
+    return "D"
+  else: 
+    return "F"
+
+def run(): 
+  ng=float(input("Enter your CMPSC 131 grade: "))
+  print("Your letter grade for CMPSC 131 is {getLetterGrade(ng)}.")
+  
+  if __name__ == "__main__":
+    run()
