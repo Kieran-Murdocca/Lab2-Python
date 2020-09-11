a0cac33453b156b9070da622f9892c2973677816
KieranMurdocca <kkm5754@psu.edu>
2020-09-11 02:04:29 +0000

    test

--- main.py
+++ main.py
@@ -5,18 +5,15 @@
 # Section: 004
 # Breakout: 18
 # Part 2: lab2-python
-import lib
 ng=float(input("Enter your CMPSC 131 grade: "))
 def getLetterGrade(ng): 
-  if ng >= float(93): print("A")
-  elif ng >= float(90): print("A-")
-  elif ng >= float(87): print("B+")
-  elif ng >= float(83): print("B")
-  elif ng >= float(80): print("B-")
-  elif ng >= float(77): print("C+")
-  elif ng >= float(70): print("C")
-  elif ng >= float(60): print("D")
-  else: print("F")
-print("Your letter grade for CMPSC 131 is ") ,getLetterGrade(ng)
-#+ "."))
-#print(getLetterGrade(ng))
+  if ng >= float(93): print("Your letter grade for CMPSC 131 is A.")
+  elif ng >= float(90): print("Your letter grade for CMPSC 131 is A-.")
+  elif ng >= float(87): print("Your letter grade for CMPSC 131 is B+.")
+  elif ng >= float(83): print("Your letter grade for CMPSC 131 is B.")
+  elif ng >= float(80): print("Your letter grade for CMPSC 131 is B-.")
+  elif ng >= float(77): print("Your letter grade for CMPSC 131 is C+.")
+  elif ng >= float(70): print("Your letter grade for CMPSC 131 is C.")
+  elif ng >= float(60): print("Your letter grade for CMPSC 131 is D.")
+  else: print("Your letter grade for CMPSC 131 is F.")
+getLetterGrade(ng)
